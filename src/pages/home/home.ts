31ae723f3155599abeb4d438810611fb5be6a269
Dheymer León <dleon@webcreek.com>
2018-02-01 17:39:57 -0500

    Added some functionality and form components

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DateTime } from 'ionic-angular/components/datetime/datetime';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  picoYPlaca = [5,1,1,2,2,3,3,4,4,5];
+  strPlate: String = '';
+  movingDate: DateTime;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  checkPicoYPlaca(plate: String, movDate: DateTime){
+    var auxDate = new Date(this.movingDate.doneText);
+    //var firstHour = auxDate; firstHour.s
+    var weekDay = auxDate.getDay();
+    var actualTime = auxDate.getTime();
+    var auxplate = parseInt(plate.substr(plate.length-1, 1));
+    if(this.picoYPlaca[auxplate] == weekDay){
+      if(((actualTime >= new Date().getTime()))){
+
+      }
+    }
   }
 
 }
