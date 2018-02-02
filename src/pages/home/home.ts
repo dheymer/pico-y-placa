b02f17cbe949d91ac145a6e88d90884933881742
Dheymer León <dheymer@gmail.com>
2018-02-02 15:03:34 -0500

    Optimized a bit and documentation of the code.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
-//import { DateTimeData } from 'ionic-angular/util/datetime-util';
 import { DateTime } from 'ionic-angular/components/datetime/datetime';
 
 @Component({
@@ -8,58 +7,61 @@ import { DateTime } from 'ionic-angular/components/datetime/datetime';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  picoYPlaca = [5,1,1,2,2,3,3,4,4,5];
-  strPlate: String = '';
-  movingDate: DateTime;
+  picoYPlaca = [5,1,1,2,2,3,3,4,4,5]; // Array to define which day of the week "Pico y Placa" will be applied to each plate number
+  strPlate: String = '';              // The plate number
+  movingDate: DateTime;               // The date and time picker
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
+  /**
+   * Method to check if "Pico y Placa" can be applied
+   */
   checkPicoYPlaca(){
-    var str = 'Chequeando Pico y Placa para el día '+this.movingDate;
-    console.log(str);
-    var alert;
-    var strDate = str.substr(36, 10);
-    var strTime = str.substr(47, 8)
-    var auxDate = new Date(strDate+' '+strTime);
-    var morningStart = new Date(strDate+' 07:00:00').getTime();
-    var morningEnd = new Date(strDate+' 09:30:00').getTime();
-    var eveningStart = new Date(strDate+' 16:00:00').getTime();
-    var eveningEnd = new Date(strDate+' 19:30:00').getTime();
-    var weekDay = auxDate.getDay();
-    var actualTime = auxDate.getTime();
-    var auxPlate = parseInt(this.strPlate.substr(this.strPlate.length-1, 1));
-    if(this.picoYPlaca[auxPlate] == weekDay){
-      if(
+    var str = ''+this.movingDate;                         // String with the value of the date and time picker
+    var alert;                                            // The alert showing the result of the check for "Pico y Placa"
+    var strDate = str.substr(0, 10);                      // The date component of the DateTime string
+    var strTime = str.substr(11, 8);                      // The time component of the DateTime string
+    var auxDate = new Date(strDate+' '+strTime);          // The date picked as object to make the operations
+    const morningStart = new Date(strDate+' 07:00:00').getTime(); // The morning starting time of "Pico y Placa"
+    const morningEnd = new Date(strDate+' 09:30:00').getTime();   // The morning ending time of "Pico y Placa"
+    const eveningStart = new Date(strDate+' 16:00:00').getTime(); // The evening starting time of "Pico y Placa"
+    const eveningEnd = new Date(strDate+' 19:30:00').getTime();   // The evening ending time of "Pico y Placa"
+    var weekDay = auxDate.getDay();                       // The day of the week in numeric format
+    var actualTime = auxDate.getTime();                   // The time format for the selected date and time
+    var auxPlate = parseInt(this.strPlate.substr(this.strPlate.length-1, 1)); // The last character of the plate number
+
+    if(this.picoYPlaca[auxPlate] == weekDay){             // If the plate number is in it's corresponding day of the week for
+      if(                                                 // "Pico y Placa"...
         (
-          (actualTime >= morningStart) &&
-          (actualTime <= morningEnd)
+          (actualTime >= morningStart) &&                 // ...check if the selected time is between the start and ending
+          (actualTime <= morningEnd)                      // time for the morning or evening.
         ) || (
           (actualTime >= eveningStart) &&
           (actualTime <= eveningEnd)
         )
-      ){
-        alert = this.alertCtrl.create({
-          title: 'No puede circular',
+      ){                                                  // ... If it is, then show the alert indicating that the vehicle
+        alert = this.alertCtrl.create({                   // can't road bacause it's the day and time for it's
+          title: 'No puede circular',                     // "Pico y Placa" restriction...
           subTitle: 'El vehículo está en el día y horarios correspondientes de Pico y Placa',
           buttons:['OK']
         });
       }else{
-        alert = this.alertCtrl.create({
-          title: 'Si puede circular',
+        alert = this.alertCtrl.create({                   // ...otherwise, show the alert indicating that the vehicle can
+          title: 'Si puede circular',                     // road because it's the day, but not the time for it's restriction.
           subTitle: 'El vehículo está en el día de Pico y Placa, pero no en el horario correspondiente',
           buttons:['OK']
         });
       }
-    }else{
-      alert = this.alertCtrl.create({
-        title: 'Si puede circular',
+    }else{                                                // If the plate number is not in it's corresponding day of the week
+      alert = this.alertCtrl.create({                     // for "Pico y Placa", then show the alert indicating that the
+        title: 'Si puede circular',                       // vehicle can road because it's out of the restriction that day.
         subTitle: 'El vehículo no está en el día correspondiente de Pico y Placa',
         buttons:['OK']
       });
     }
-    console.log(
+    console.log(                                          // Console dump of the data used (debugging purposes only)
       'auxDate: '+ auxDate+
       '\nweekDay: '+weekDay+
       '\nactualTime:'+actualTime+
@@ -71,6 +73,6 @@ export class HomePage {
       '\npicoYPlaca: '+this.picoYPlaca+
       '\npicoYPlaca[auxPlate]:'+this.picoYPlaca[auxPlate]
     );
-    alert.present();
+    alert.present();                                      // Show the alert indicating the result.
   }
 }
